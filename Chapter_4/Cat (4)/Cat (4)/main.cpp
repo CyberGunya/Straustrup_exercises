e60b09fbcc4bf0cf54be13dd87b0d6fe862fbfa3
Serhii Hlushenkov <gunya@MacBook-Pro-Sergej.local>
2022-09-29 16:45:53 +0300

    o

--- Chapter_4/Cat (4)/Cat (4)/main.cpp
+++ Chapter_4/Cat (4)/Cat (4)/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::ifstream input;
+    for(int i=1;i<argc;i++)
+    {
+        std::string file = "/Users/gunya/Desktop/Learn_C++/Chapter_4/Cat (4)/Cat (4)/";
+        // Enter the path, where mentioned files are located
+        file+=argv[i];
+        input.open(file);
+        if(input.is_open())
+        {
+            while(input)
+            {
+                file=input.get();
+                if(file!="\377")
+                {
+                    std::cout<<file;
+                }
+            }
+        }
+        std::cout<<"\n";
+        input.close();
+    }
     return 0;
 }
---
+++ Chapter_4/Heap sort (6)/Heap sort (6)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Heap sort (6)
+//
+//  Created by Serhii Hlushenkov on 29.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
