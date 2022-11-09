d8bc5622dbf901ac65ca7b5ff74db0220e3ea7c9
Serhii Hlushenkov <gunya@MacBook-Pro-Sergej.local>
2022-11-09 20:35:15 +0200

    Added distributions

---
+++ Chapter_5/Distributions (8)/Distributions (8)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Distributions (8)
+//
+//  Created by Serhii Hlushenkov on 09.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
