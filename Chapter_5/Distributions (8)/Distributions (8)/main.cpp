b2b67cb43d2054d3c11bf8fdb5a50eefeda9de55
Serhii Hlushenkov <gunya@MacBook-Pro-Sergej.local>
2022-11-09 20:36:35 +0200

    Update distributions

--- Chapter_5/Distributions (8)/Distributions (8)/main.cpp
+++ Chapter_5/Distributions (8)/Distributions (8)/main.cpp
@@ -6,9 +6,82 @@
 //
 
 #include <iostream>
+#include <random>
+#include <cmath>
+
+std::mt19937 gen32 (time(0));
+std::uniform_real_distribution<double> dis(0, 1);
+
+class uniform{
+    double left, right;
+public:
+    uniform(double a, double b);
+    double draw();
+};
+
+uniform::uniform(double a, double b)
+{
+    if(a>b)
+    {
+        std::cout<<"Second value must be greater! Values will be switched."<<"\n";
+        left=b;
+        right=a;
+    }
+    else
+    {
+        left=a;
+        right=b;
+    }
+}
+
+double uniform::draw()
+{
+    return left+dis(gen32)*(right-left);
+}
+
+
+class normal{
+    double alpha, sigma;
+public:
+    normal(double a, double s);
+    double draw();
+};
+
+normal::normal(double a, double s)
+{
+    alpha=a;
+    sigma=s;
+}
+
+double normal::draw()
+{
+    double x = sqrt(-2*log(dis(gen32)))*sin(2*M_PI*dis(gen32));
+    return alpha+sigma*x;
+}
+
+class exponential{
+    double lambda;
+public:
+    exponential(double l);
+    double draw();
+};
+
+exponential::exponential(double l)
+{
+    lambda=l;
+}
+
+double exponential::draw()
+{
+    return log(1-dis(gen32))/(-lambda);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    uniform first(5,10);
+    normal second(7, 1.5);
+    exponential third(1.5);
+    
+    std::cout<<first.draw()<<"\n"<<second.draw()<<"\n"<<third.draw()<<"\n";
     return 0;
 }
