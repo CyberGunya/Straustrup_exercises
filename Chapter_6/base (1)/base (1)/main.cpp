20ae892de2609153dc75310f2c4ec609e9ead9c2
Serhii Hlushenkov <gunya@MacBook-Pro-Sergej.local>
2022-11-10 22:19:14 +0200

    6 chapter start

--- Chapter_6/base (1)/base (1)/main.cpp
+++ Chapter_6/base (1)/base (1)/main.cpp
@@ -7,8 +7,31 @@
 
 #include <iostream>
 
+class base{
+public:
+    virtual void iam() { std::cout<< "base\n"; }
+};
+
+class base1 : public base{
+public:
+    void iam() { std::cout<<"base1\n"; }
+};
+
+class base2 : public base{
+public:
+    void iam() { std::cout<<"base2\n"; }
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    base b;
+    base1 b1;
+    base2 b2;
+    b.iam();
+    b1.iam();
+    b2.iam();
+    base* temp = &b1;
+    temp->iam();
+    temp = &b2;
+    temp->iam();
     return 0;
 }
