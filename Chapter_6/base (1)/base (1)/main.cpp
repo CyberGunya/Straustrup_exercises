499f8fe1fa5c5891827b567a5701445f63efab24
Serhii Hlushenkov <gunya@MacBook-Pro-Sergej.local>
2022-11-10 22:17:35 +0200

    6 chapter start

---
+++ Chapter_6/base (1)/base (1)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  base (1)
+//
+//  Created by Serhii Hlushenkov on 10.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
